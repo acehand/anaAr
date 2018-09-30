6bfcf58abd6ec61bdd2dfda9d5f537279939f9d8
Anand Chandrasekaran <anandc.1988@gmail.com>
2018-09-30 15:32:43 -0400

    - React Load Updates - Includes HOme.js for the homecontent - -   - Example Folder Updates     - all files are now reletivel accesed via accurate folder and file names (assets, css, and js respectively)    - Pages and all sources work as expected    - WIP need to remove GA.js, useless analytics f

---
+++ examples/project_anaar/src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component{
+  render() {
+    return <section id="title" class="panel inview" artist="Jessica Hische" href="http://www.jessicahische.is" style="height: 800px;"> <!-- 1 -->
+			<div class="b" style="height: 800px;"></div>
+      <div class="caption invisible">
+        <p>The Story of Russ and Jessica</p>
+      </div>
+    </section>
+  }
+}
+export default Home;
