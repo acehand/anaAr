63c244c970e591a82ad6f0aadee797dadb1c858b
Anand Chandrasekaran <anandc.1988@gmail.com>
2018-10-03 07:12:09 -0400

    Uploading GMaps Locatio

--- examples/project_anaar/src/sections.js
+++ examples/project_anaar/src/sections.js
@@ -10,7 +10,7 @@ class Contentsections extends React.Component{
     super(props);
     this.state = { 
       isToggleOn: true, 
-      offsetLen: 2,
+      offsetLen: 5,
       loadNavigation:false,
     };
     // This binding is necessary to make `this` work in the callback
@@ -39,7 +39,7 @@ class Contentsections extends React.Component{
     return (
       <div>
           {this._renderNavigation()}
-        <Parallax ref={ref => (this.parallax = ref)} pages={4}>
+        <Parallax ref={ref => (this.parallax = ref)} pages={5}>
           <Parallax.Layer className="subK" factor={1.25} offset={0} speed={.25}  onClick={e => this.handleClick(this.parallax)}>
             <section id="sOne" class="img-fullscreen">
               <SnowStorm snowColor="green" targetElement="sOne" snowCharacter="~^~"/>
@@ -52,7 +52,7 @@ class Contentsections extends React.Component{
               <SectionTwo />
             </section>
           </Parallax.Layer>
-          <Parallax.Layer factor={1} offset={2} speed={.25} onClick={e => this.handleClick(this.parallax)}>
+          <Parallax.Layer factor={1} offset={2} speed={1} onClick={e => this.handleClick(this.parallax)}>
           <section id="sThree">
               <SnowStorm snowColor="White" targetElement="sThree" snowCharacter="**" />      
               <SectionThree/>
@@ -63,6 +63,14 @@ class Contentsections extends React.Component{
               <SnowStorm snowColor="White" targetElement="sThree" snowCharacter="**" />
               <SectionFour />
             </section>
+          </Parallax.Layer>
+          <Parallax.Layer factor={1} offset={4} speed={.25} onClick={e => this.handleClick(this.parallax)}>
+          <section id="sFive">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.576894994789!2d80.25271465034191!3d12.934893219137827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525cdd5d90609f%3A0xf6d497e08ae5e492!2sThe+Royal+Palms!5e0!3m2!1sen!2sus!4v1538564720063"
+                frameborder="0" style={{ border: "0" }} allowfullscreen></iframe>
+          </section>
+          
           </Parallax.Layer>
         </Parallax>
       </div>
