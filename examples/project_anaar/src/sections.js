7a4b4ebbbfc4ef67a2a1085c1f2c92f960f9c2db
Anand Chandrasekaran <anandc.1988@gmail.com>
2018-10-03 03:48:53 -0400

    Page 1 doen

--- examples/project_anaar/src/sections.js
+++ examples/project_anaar/src/sections.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Spring, Parallax, ParallaxLayer } from 'react-spring';
+import { Container, Row, Col } from 'reactstrap';
 import Navigate from './nav';
 
 class Contentsections extends React.Component{  
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: true, offsetLen: 2, loadNavigation:false};
+    this.state = { 
+      isToggleOn: true, 
+      offsetLen: 2, 
+      loadNavigation:false,
+      heading1 : "Anand & Archana",
+      subHeading1: "Few years ago, they didnt know what they were meant to be",
+      subHeading2: "Now, they are all geared up to exchange rings, party around and get married",
+      eventDate : "Sunday, December 9th 2018"
+    };
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
     this._renderNavigation = this._renderNavigation.bind(this);
@@ -16,8 +25,11 @@ class Contentsections extends React.Component{
   _renderNavigation() {
     if (this.state.loadNavigation) {
       return <Navigate />;
-    }    
+    }   
   }
+  // _loadSections() {
+  //   let 
+  // }
   handleClick(parallax) {
     let currentOffset = parallax.offset;
     if (currentOffset == this.state.offsetLen) {
@@ -28,14 +40,27 @@ class Contentsections extends React.Component{
   render() {
     return (
       <div>
-        {this._renderNavigation(this.state.loadNavigation)}
-        <Parallax ref={ref => (this.parallax = ref)} pages={5} >
-          <Parallax.Layer id="sOne" factor={1.5} offset={0} speed={.5} style={{ backgroundColor: 'teal' }} onClick={e => this.handleClick(this.parallax)}>
-          </Parallax.Layer>
-          <Parallax.Layer id="sTwo" factor={1.5} offset={1} speed={.25} style={{ backgroundColor: 'PInk' }} onClick={e => this.handleClick(this.parallax)}>
-          </Parallax.Layer>
-          <Parallax.Layer id="sThree" factor={1.5} offset={2} speed={.25} style={{ backgroundColor: 'brown' }} onClick={e => this.handleClick(this.parallax)}>
-            
+        {this._renderNavigation()}
+        <Parallax ref={ref => (this.parallax = ref)} pages={3} >
+          <Parallax.Layer id="sOne" factor={1} offset={0} speed={.25}  onClick={e => this.handleClick(this.parallax)}>
+            <section id="sOne" class="img-fullscreen">
+              <Container className="align-center">
+                <Row>
+                  <Col md={{size:10}} className="fWhite">
+                      <h1 className="section-title">
+                        Anand &amp; Archana
+                      </h1>
+                      <h3 className="section-subtitle  mbr-light">
+                        <span>{this.state.subHeading1}</span><br/>
+                      <span>{this.state.subHeading2}</span>
+                      </h3>
+                      <p class="section-text display-7">
+                        {this.state.eventDate}
+                      </p>
+                  </Col>
+                </Row>
+              </Container>
+            </section>
           </Parallax.Layer>
         </Parallax>
       </div>
