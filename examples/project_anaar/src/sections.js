976e85eaeff6b89df10cb02bf70f3c57c51cfe9d
Anand Chandrasekaran <anandc.1988@gmail.com>
2018-10-31 11:53:18 -0400

    Map sections

--- examples/project_anaar/src/sections.js
+++ examples/project_anaar/src/sections.js
@@ -102,10 +102,14 @@ class Contentsections extends React.Component{
 
           <Parallax.Layer factor={1} offset={5.6} speed={0} onClick={e => this.handleClick(this.parallax,5.4)}>
             <section id="sSeven">
-              <iframe
-                title="maps"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.576894994789!2d80.25271465034191!3d12.934893219137827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525cdd5d90609f%3A0xf6d497e08ae5e492!2sThe+Royal+Palms!5e0!3m2!1sen!2sus!4v1538564720063"
-                frameborder="0" style={{ border: "0" }} allowfullscreen></iframe>
+              <div className="d-flex flex-column w-100 h-100">
+                <div className="align-items-center mb-auto h-100">
+                  <iframe
+                    title="maps"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.576894994789!2d80.25271465034191!3d12.934893219137827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525cdd5d90609f%3A0xf6d497e08ae5e492!2sThe+Royal+Palms!5e0!3m2!1sen!2sus!4v1538564720063"
+                    frameborder="0" style={{ border: "0" }} allowfullscreen></iframe>
+                </div>
+              </div>
             </section>
           </Parallax.Layer>
           </Parallax>
