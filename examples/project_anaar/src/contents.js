9af7d6ec2e6e92788293704aa12f322dbe8afd05
Anand Chandrasekaran <anandc.1988@gmail.com>
2018-11-14 04:16:45 -0500

    Fixing CSS for ipad pro and responsive

--- examples/project_anaar/src/contents.js
+++ examples/project_anaar/src/contents.js
@@ -32,10 +32,10 @@ export class SectionOne extends React.Component {
 
 const AnimatedContent = ({left, right}) => {
   return (<p className="section-subtitle display-linebreak">
-    <Animation className="reveal d-inline-block" type="slideInLeft" duration="3s" delay="2s">
+    <Animation className="reveal d-inline-block" type="slideInLeft" duration="3s" delay="4s">
       {left}
     </Animation>
-    <Animation className="d-inline-block" type="slideInRight" duration="3s" delay="2s">
+    <Animation className="d-inline-block" type="slideInRight" duration="3s" delay="4s">
       {right}
     </Animation>
     </p>
@@ -47,15 +47,15 @@ export class SectionTwo extends React.Component {
     super(props);
     this.state = {
       heading1: "One fine fall week...",
-      left0: "He bragged about NYC's work life.",
+      left0: "He bragged about NYC's work life.\xA0",
       right0: "She went there to see it for herself. ",
-      left1: "He made her laugh. ",
+      left1: "He made her laugh.\xA0",
       right1: "She made him work lesser. ",
-      left2: "He played tricks on her. ",
-      right2: " She teased him by not completing her stories. ",
-      left3: "He woke her up every morning. ",
-      right3: "She slept longer. ",
-      left4: "He talked and talked and talked. ",
+      left2: "He played tricks on her.\xA0",
+      right2: "She teased him by not completing her stories.",
+      left3: "He woke her up every morning.\xA0",
+      right3: "She slept longer.",
+      left4: "He talked and talked and talked.\xA0",
       right4: "She listened!",
     }
   }
@@ -120,11 +120,11 @@ export class SectionFive extends React.Component {
       inviteSub1: "As we team up for the game of life,",
       inviteSub2: "You are invited to brighten up the \n next page of our story by",
 
-      reception1: "#1 Making the reception fun by dancing \n with us on",
-      receptionDate: "7pm Saturday\n 8th of December 2018",
+      reception1: "#1 Making the reception fun by dancing \n with us at",
+      receptionDate: "7pm on Saturday\n 8th of December 2018",
 
-      wedding1: "#2 Cheering at our traditional wedding \n ceremony on",
-      weddingDate: "8am Sunday\n 9th of December 2018",
+      wedding1: "#2 Cheering at our traditional wedding \n ceremony at",
+      weddingDate: "8am on Sunday\n 9th of December 2018",
 
     }
   }
