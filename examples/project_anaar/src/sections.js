fab906df66ac4b9a6ca2e632a68264d6ebe60a48
Anand Chandrasekaran <anandc.1988@gmail.com>
2018-11-14 02:50:54 -0500

    Bouqet message

--- examples/project_anaar/src/sections.js
+++ examples/project_anaar/src/sections.js
@@ -142,6 +142,9 @@ class Contentsections extends React.Component{
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.576894994789!2d80.25271465034191!3d12.934893219137827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525cdd5d90609f%3A0xf6d497e08ae5e492!2sThe+Royal+Palms!5e0!3m2!1sen!2sus!4v1538564720063"
                   frameborder="0" style={{ border: "0" }} allowfullscreen></iframe>
               <div className="d-flex section-subtitle white-text justify-content-center flex-column mt-5">
+                <span className="contact">bearing no gifts or bouquets :)</span>
+              </div>
+              <div className="d-flex section-subtitle white-text justify-content-center flex-column mt-5">
                 <span className="contact">Contact - Anand  +916369800409/lelouchitis@gmail.com</span>
               </div>
                 </section>
